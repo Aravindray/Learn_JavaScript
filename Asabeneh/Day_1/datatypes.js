c2e639a1f58cc4d12e666784a6ef78c6b91e9a67
Aravindray <aravindselvasan@gmail.com>
2024-05-01 20:19:02 +0530

    changed the folder name

---
+++ Asabeneh/Day_1/datatypes.js
@@ -0,0 +1,40 @@
+/* Data Type in JavaScript 
+String,
+Number, 
+Boolean,
+Undefined, 
+Null and
+Symbol
+*/
+
+// Numbers
+/* 
+Integer: -3, -2, -1, 0, 1, 2, 3
+Floating Points: -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0
+*/
+
+// Strings
+/* 
+'a'
+'Aravind'
+`Javascript is a beautiful programming language`
+"And we also use double quotes"
+`backticks`
+*/
+
+// Booleans
+// True or False
+
+// Undefined
+// Variables with values, or a function without return anything.
+
+// Null
+// An empty value
+
+// To check the variable type use the typeof operator
+
+console.log(typeof 'Aravind')
+console.log(typeof 15)
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
