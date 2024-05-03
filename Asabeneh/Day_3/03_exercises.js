1ad975406b3377601cacc749bcc5cf08b77e3327
Aravindray <aravindselvasan@gmail.com>
2024-05-03 19:40:14 +0530

    working on day 3 challenges

---
+++ Asabeneh/Day_3/03_exercises.js
@@ -0,0 +1,95 @@
+// Level 1
+
+console.log('// Level 1')
+
+let firstName = 'Aravind'
+let lastName = 'ray'
+let planet = 'Earth'
+let city = 'Salem'
+let age = 24
+let isMarried = false
+let bornYear = 1999
+
+console.log('---Question 1---')
+
+console.log('typeof firstName //',typeof firstName)
+console.log('typeof lastName //',typeof lastName)
+console.log('typeof planet //',typeof planet)
+console.log('typeof city //',typeof city)
+console.log('typeof age //',typeof age)
+console.log('typeof isMarried //',typeof isMarried)
+console.log('typeof bornYear //',typeof bornYear)
+
+console.log('---Question 2---')
+
+console.log("typeof '10' == typeof 10 //",typeof '10' == typeof 10)
+
+console.log('---Question 3---')
+
+console.log("parseInt('9.8') == 10 //",parseInt('9.8') == 10)
+
+console.log('---Question 4---')
+
+console.log("firstName.includes('vi') //",firstName.includes('vi'))
+console.log("typeof bornYear == 'number' //",typeof bornYear == 'number')
+console.log("'10' == 10 //",'10' == 10)
+console.log()
+console.log("'10' === 10 //",'10' === 10)
+console.log("isMarried == true //",isMarried == true)
+console.log("'10.0' != 10.0 //",'10.0' != 10.0)
+
+console.log('---Question 5---')
+
+console.log("4 > 3 //",4 > 3)
+console.log("4 >= 3 //",4 >= 3)
+console.log("4 < 3 //",4 < 3)
+console.log("4 <= 3 //",4 <= 3)
+console.log("4 == 4 //",4 == 4)
+console.log("4 === 4 //",4 === 4)
+console.log("4 != 4 //",4 != 4)
+console.log("4 !== 4 //",4 !== 4)
+console.log("4 != '4' //",4 != '4')
+console.log("4 == '4' //",4 == '4')
+console.log("4 === '4' //",4 === '4')
+console.log("'python'.length < 'jargon'.length //",'python'.length < 'jargon'.length)
+
+console.log('---Question 6---')
+
+console.log('4 > 3 && 10 < 12 //',4 > 3 && 10 < 12)
+console.log('4 > 3 && 10 > 12 //',4 > 3 && 10 > 12)
+console.log('4 > 3 || 10 < 12 //',4 > 3 || 10 < 12)
+console.log('4 > 3 || 10 > 12 //',4 > 3 || 10 > 12)
+console.log('!(4 > 3) //',!(4 > 3))
+console.log('!(4 < 3) //',!(4 < 3))
+console.log('!(false) //',!(false))
+console.log('!(4 > 3 && 10 < 12) //',!(4 > 3 && 10 < 12))
+console.log('!(4 > 3 && 10 > 12) //',!(4 > 3 && 10 > 12))
+console.log("!(4 === '4') //",!(4 === '4'))
+console.log("!('dragon'.includes('on') && 'python'.includes('on') //",!('dragon'.includes('on') && 'python'.includes('on')))
+
+console.log('---Question 7---')
+
+let now = new Date
+console.log('Current Year is',now.getFullYear())
+console.log('Current Month is',now.getMonth() + 1)
+console.log('Today\'s Date is',now.getDate())
+console.log('Current Week Day is',now.getDay())
+console.log('Current Hour is',now.getHours())
+console.log('Current Minute is',now.getMinutes())
+console.log('Time in seconds from Jan 1, 1970 to now',now.getTime())
+console.log()
+
+// Level 2
+
+console.log('// Level 2')
+/*
+console.log('---Question 1---')
+
+let base = prompt('Enter the base of the triangle')
+let height = prompt('Enter the height of the triangle')
+let areaTriangle = 0.5 * base * height
+alert(`Entered base is ${base}cm and height is ${height}cm and area of triangle is ${areaTriangle}cm`)
+
+*/
+
+console.log('---Question 5---')
