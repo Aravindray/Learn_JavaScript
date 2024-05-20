8654a998c3740da7022cf3cfe4848d31f83c77ac
Aravindray <aravindselvasan@gmail.com>
2024-05-20 17:35:58 +0530

    completed day 1 exercises

--- Asabeneh/01_Introduction/01_datatypes.js
+++ Asabeneh/01_Introduction/01_datatypes.js
@@ -38,3 +38,22 @@ console.log(typeof 15)
 console.log(typeof true)
 console.log(typeof null)
 console.log(typeof undefined)
+
+
+// Exercises
+
+console.log('Exercises')
+
+let firstName = 'Aravindray'
+let isMarried = false
+let age = 25
+let whatHeIsDoing
+let empty = null
+let extraDigits = 238938943939284702394802n
+
+console.log('Type of', firstName, 'is', typeof firstName)
+console.log('Type of', isMarried, 'is', typeof isMarried)
+console.log('Type of', age, 'is', typeof age)
+console.log('Type of', whatHeIsDoing, 'is', typeof whatHeIsDoing)
+console.log('Type of', empty, 'is', typeof empty)
+console.log('Type of', extraDigits, 'is', typeof extraDigits)
