e6c25ba11ef5ed313e02a0f074e2e59adba459f8
Aravindray <aravindselvasan@gmail.com>
2024-05-20 19:51:25 +0530

    working on day 2 exercises

--- Asabeneh/02_Data_Types/02_exercises.js
+++ Asabeneh/02_Data_Types/02_exercises.js
@@ -1,12 +1,106 @@
+// Level 1
+
+console.log('Level 1')
+
+console.log('---Question 1---')
+let challenge = '30 Days of JavaScript'
+
+console.log('---Question 2---')
+console.log(challenge)
+
+console.log('---Question 3---')
+console.log(challenge.length)
+
+console.log('---Question 4---')
+console.log(challenge.toUpperCase())
+
+console.log('---Question 5---')
+console.log(challenge.toLowerCase())
+
+console.log('---Question 6---')
+console.log(challenge.substring(0,2))
+
+console.log('---Question 7---')
+let findIndex = challenge.indexOf('Days')
+console.log(challenge.substring(findIndex))
+
+console.log('---Question 8---')
+console.log(challenge.includes('Script'))
+
+console.log('---Question 9---')
+console.log(challenge.split(''))
+
+console.log('---Question 10---')
+console.log(challenge.split(' '))
+
+console.log('---Question 11---')
+let companies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(companies.split(', '))
+
+console.log('---Question 12---')
+console.log(challenge.replace('JavaScript', 'Python'))
+
+console.log('---Question 13---')
+console.log(challenge.charAt(15))
+
+console.log('---Question 14---')
+let indexOfJ = challenge.indexOf('J')
+console.log(challenge.charCodeAt(indexOfJ))
+
+console.log('---Question 15---')
+console.log(challenge.indexOf('a'))
+
+console.log('---Question 16---')
+console.log(challenge.lastIndexOf('a'))
+
+console.log('---Question 17---')
+let because = 'You cannot end a sentence with because because because is a conjunction'
+console.log(because.indexOf('because'))
+
+console.log('---Question 18---')
+console.log(because.lastIndexOf('because'))
+
+console.log('---Question 19---')
+console.log(because.search('because'))
+
+console.log('---Question 20---')
+let newChallenge = '30 Days of JavaScript '
+console.log(newChallenge, 'Without trim so length is', newChallenge.length)
+let newChallengeTrim = newChallenge.trim()
+console.log(newChallengeTrim, 'With trim so length is', newChallengeTrim.length)
+
+console.log('---Question 21---')
+console.log(challenge.startsWith('30'))
+
+console.log('---Question 22---')
+console.log(challenge.endsWith('Script'))
+
+console.log('---Question 23---')
+let as = challenge.match(/a/g)
+console.log(`The occurrence of letter "a" in "${challenge}" is ${as.length}`)
+
+console.log('---Question 24---')
+let firstHalf = '30 Days of'
+let secondHalf = 'JavaScript'
+let combined = firstHalf.concat(' ', secondHalf)
+console.log(combined)
+
+console.log('---Question 25---')
+console.log(newChallenge.repeat(2))
+
 // Level 2
 
-console.log()
+console.log('Level 2')
+
+console.log('---Question 1---')
 console.log('The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another.')
-console.log()
+
+console.log('---Question 2---')
 console.log('\"Love is not patronizing and charity isn\'t about pity, it is about love. Charity and love are the same -- with charity you give love, so don\'t just give money but reach out your hand instead.\"')
-console.log()
+
+console.log('---Question 3---')
 console.log(typeof +'10')
-console.log()
+
 console.log(Math.ceil(parseFloat('9.8')))
 console.log()
 let py = 'Python'
