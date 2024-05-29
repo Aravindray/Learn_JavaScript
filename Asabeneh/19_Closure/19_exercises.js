a7a6979395edff8cb85f7f7dbb0d731619140ccd
Aravindray <aravindselvasan@gmail.com>
2024-05-29 11:23:19 +0530

    completed day 19 exercises

--- Asabeneh/19_Closure/19_exercises.js
+++ Asabeneh/19_Closure/19_exercises.js
@@ -30,18 +30,96 @@ function justOuterFun() {
         return count
     }
     function secondInnerFun() {
-        return count++
+        count++
+        return count
     }
     function thirdInnerFun() {
-        return count--
+        count--
+        return count
     }
     return {
-        first: firstInnerFun(),
-        second: secondInnerFun(),
-        third: thirdInnerFun()
+        first: firstInnerFun,
+        second: secondInnerFun,
+        third: thirdInnerFun
     }
 }
 let someNewVariable = justOuterFun()
-console.log(someNewVariable.first)
-console.log(someNewVariable.first)
-console.log(someNewVariable.first)
+console.log(someNewVariable.first())
+console.log(someNewVariable.first())
+console.log(someNewVariable.first())
+
+// Level 3
+
+console.log('Level 3')
+
+console.log('---Question 1---')
+
+function personAccount(fName, lName) {
+    let firstName = fName
+    let lastName = lName
+    let incomes = []
+    let expenses = []
+
+    function totalIncome() {
+        let total = 0
+        if (incomes.length > 0) {
+            for (const income of incomes) {
+                let someThing = [...income]
+                total += someThing[0]
+            }
+        }
+        return total
+    }
+
+    function totalExpenses() {
+        let total = 0
+        if (expenses.length > 0) {
+            for (const expense of expenses) {
+                let something = [...expense]
+                total += something[0]
+            }
+        }
+        return total
+    }
+
+    function accountInfo() {
+        return `Name: ${firstName} ${lastName}\nBalance: ${accountBalance()}`
+    }
+
+    function addIncome(amount, description) {
+        let someSet = new Set()
+        someSet.add(amount)
+        someSet.add(description)
+        incomes.push(someSet)
+        console.log('Income added')
+    }
+
+    function addExpense(amount, description) {
+        let someSet = new Set()
+        someSet.add(amount)
+        someSet.add(description)
+        expenses.push(someSet)
+        console.log('Expenses added')
+    }
+
+    function accountBalance() {
+        let balance = totalIncome() - totalExpenses()
+        return balance
+    }
+
+    return {
+        totalIncome,
+        totalExpenses,
+        accountInfo,
+        addIncome,
+        addExpense,
+        accountBalance
+    }
+}
+
+let myAccount = personAccount('Money','Bell')
+myAccount.addIncome(500, 'From Microsoft')
+myAccount.addIncome(5000, 'From Dad')
+console.log(myAccount.accountInfo())
+myAccount.addExpense(1000, 'For Snacks')
+console.log(myAccount.accountInfo())
