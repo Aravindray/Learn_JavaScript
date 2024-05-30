b32228b0fc5a16a9332116513b43dc1a342941d2
Aravindray <aravindselvasan@gmail.com>
2024-05-30 22:54:39 +0530

    working on exercises

---
+++ Asabeneh/22_Manipulating_DOM_Object/22_exercises_level_2.js
@@ -0,0 +1,11 @@
+import { countries } from "./22_countries.js"
+
+// Center the element
+
+let body = document.body
+body.style.display = 'flex'
+body.style.flexDirection = 'column'
+body.style.justifyContent = 'center'
+body.style.alignItems = 'center'
+
+console.log(countries)
