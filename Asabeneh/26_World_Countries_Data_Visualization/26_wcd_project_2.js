734915f96169efa09e755836a4de6fab49d35f4a
Aravindray <aravindselvasan@gmail.com>
2024-06-03 22:48:02 +0530

    completed day 26 project

--- Asabeneh/26_World_Countries_Data_Visualization/26_wcd_project_2.js
+++ Asabeneh/26_World_Countries_Data_Visualization/26_wcd_project_2.js
@@ -9,8 +9,10 @@ async function fetchCountries() {
     }
 }
 
+let result
+
 (async () => {
-    let result = await fetchCountries()
+    result = await fetchCountries()
     let countriesCount = document.querySelector('#countriesCount')
     countriesCount.textContent = `${result.length}`
     displayResult(result)
@@ -19,6 +21,7 @@ async function fetchCountries() {
 let listOfCountries = document.querySelector('#listOfCountries')
 
 function displayResult(arr) {
+    listOfCountries.innerHTML = ''
     for (let country of arr) {
         let newDiv = document.createElement('div')
         newDiv.textContent = country
@@ -30,22 +33,76 @@ function displayResult(arr) {
 // main function
 
 let matchFirst = document.querySelector('#matchFirst')
+let searchQuery = document.querySelector('#searchQuery')
+let matchAll = document.querySelector('#matchAll')
 
-// function userInput() {
-//     let searchCountries = document.querySelector('#searchCountries')
-//     searchCountries.addEventListener('input', (e) => {
-//         console.log(e.target.value)
-//         return e.target.value
-//     })
-// }
+matchFirst.addEventListener('click', () => {
+    matchFirst.style.backgroundColor = 'blueviolet'
+    matchFirst.style.borderColor = 'blueviolet'
+    matchAll.style.backgroundColor = 'lightskyblue'
+    matchAll.style.borderColor = 'lightskyblue'
+    matchFirstResult()
+})
 
-// matchFirst.addEventListener('click', async () => {
-//     matchFirst.style.backgroundColor = 'blueviolet'
-//     listOfCountries.innerHTML = ''
-//     let countries = await fetchCountries()
-//     let value = userInput()
-//     let result = countries.filter(country => {
-//         return country.startsWith(value)
-//     })
-//     console.log(result)
-// })
+function matchFirstResult() {
+    let searchCountries = document.querySelector('#searchCountries')
+    searchCountries.addEventListener('input', e => {
+        let value = e.target.value
+        let queryOutput = result.filter(ele => ele.toLowerCase().startsWith(value.toLowerCase()))
+        if (!(value === '')) {
+            searchQuery.textContent = `Countries starts with "${value}" are ${queryOutput.length}`
+        } else {
+            searchQuery.textContent = ''
+        }
+        displayResult(queryOutput)
+    })
+}
+
+let sorting = document.querySelector('#sorting')
+
+let initialState = false
+
+sorting.addEventListener('click', () => {
+    matchFirst.style.backgroundColor = 'lightskyblue'
+    matchFirst.style.borderColor = 'lightskyblue'
+    matchAll.style.backgroundColor = 'lightskyblue'
+    matchAll.style.borderColor = 'lightskyblue'
+    if(!initialState) {
+        sorting.style.backgroundColor = 'blueviolet'
+        sorting.style.borderColor = 'blueviolet'
+        initialState = true
+        let zToa = [...result]
+        zToa.sort()
+        zToa.reverse()
+        displayResult(zToa)
+    } else {
+        sorting.style.backgroundColor = 'lightskyblue'
+        sorting.style.borderColor = 'lightskyblue'
+        initialState = false
+        let aToz = [...result]
+        aToz.sort()
+        displayResult(aToz)
+    }
+})
+
+matchAll.addEventListener('click', () => {
+    matchAll.style.backgroundColor = 'blueviolet'
+    matchAll.style.borderColor = 'blueviolet'
+    matchFirst.style.backgroundColor = 'lightskyblue'
+    matchFirst.style.borderColor = 'lightskyblue'
+    matchAllResult()
+})
+
+function matchAllResult() {
+    let searchCountries = document.querySelector('#searchCountries')
+    searchCountries.addEventListener('input', e => {
+        let value = e.target.value
+        let queryOutput = result.filter(ele => ele.toLowerCase().includes(value.toLowerCase()))
+        if (!(value === '')) {
+            searchQuery.textContent = `Countries contains "${value}" are ${queryOutput.length}`
+        } else {
+            searchQuery.textContent = ''
+        }
+        displayResult(queryOutput)
+    })
+}
