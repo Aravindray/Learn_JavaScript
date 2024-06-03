cb63de9b14f3ce424b74544a992df7d6ac1d6ea9
Aravindray <aravindselvasan@gmail.com>
2024-06-03 19:46:07 +0530

    working on project

--- Asabeneh/26_World_Countries_Data_Visualization/26_wcd_project_2.js
+++ Asabeneh/26_World_Countries_Data_Visualization/26_wcd_project_2.js
@@ -1,6 +1,38 @@
 import { returnCountries } from "./26_countries_data.js";
 
 async function fetchCountries() {
-    let countries = await returnCountries()
-    return countries
+    try {
+        let countries = await returnCountries()
+        return countries
+    } catch(error) {
+        console.log(error)
+    }
 }
+
+(async () => {
+    let result = await fetchCountries()
+    let countriesCount = document.querySelector('#countriesCount')
+    countriesCount.textContent = `${result.length}`
+    displayResult(result)
+})()
+
+let listOfCountries = document.querySelector('#listOfCountries')
+
+function displayResult(arr) {
+    for (let country of arr) {
+        let newDiv = document.createElement('div')
+        newDiv.textContent = country
+        newDiv.className = 'card'
+        listOfCountries.appendChild(newDiv)
+    }
+}
+
+// main function
+
+let matchFirst = document.querySelector('#matchFirst')
+
+matchFirst.addEventListener('click', async (e) => {
+    listOfCountries.innerHTML = ''
+    let value = e.target.value
+    let countries = await fetchCountries()
+})
