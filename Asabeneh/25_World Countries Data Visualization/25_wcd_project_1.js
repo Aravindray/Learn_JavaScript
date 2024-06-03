365915288a5ef4d869eca2b59c7dea1954cd5c3b
Aravindray <aravindselvasan@gmail.com>
2024-06-03 11:45:13 +0530

    completed day 25 project

--- Asabeneh/25_World Countries Data Visualization/25_wcd_project_1.js
+++ Asabeneh/25_World Countries Data Visualization/25_wcd_project_1.js
@@ -9,10 +9,11 @@ let btnStatus = document.querySelector('#btnStatus')
 let visual = document.querySelector('#visual')
 
 let countriesCount = []
-let languagesCount = []
+let languagesCountWithDuplicate = []
+let actualLanguageCount = []
 let totalPopulation = 0
 
-// functions
+// functions for countries
 
 function appendCountries() {
     for (let country of countries_data) {
@@ -27,14 +28,6 @@ function appendCountries() {
     })
 }
 
-function appendLanguages() {
-    for (let country of countries_data) {
-        for (let lang of country.languages) {
-            languagesCount.lang(lang)
-        }
-    }
-}
-
 function totalPop() {
     appendCountries()
     for (let data of countriesCount) {
@@ -46,22 +39,19 @@ function totalPop() {
     }
     countriesCount.unshift(obj)
 }
+totalPop()
 
 function rtnPercentage(value, total) {
     let part = value
-    // console.log('part', part)
     let whole = total
-    // console.log('whole', whole)
     let result = (part / whole) * 100
-    // console.log('result', Math.round(result))
     return Math.round(result)
 }
 
-// event listeners
+// event listeners for country button
 
 pop.addEventListener('click', () => {
     visual.innerHTML = ''
-    totalPop()
     let topTen = countriesCount.slice(0, 11)
     let total = topTen[0].population
     for (let ele of topTen) {
@@ -84,4 +74,54 @@ pop.addEventListener('click', () => {
     }
     countStatus.textContent = `${countriesCount.length - 1} Countries`
     btnStatus.textContent = '10 Most Populated Countries in the world'    
+})
+
+// functions for languages
+
+function appendLanguages() {
+    for (let country of countries_data) {
+        for (let lang of country.languages) {
+            languagesCountWithDuplicate.push(lang)
+        }
+    }
+}
+appendLanguages()
+
+let setOfLanguages = new Set(languagesCountWithDuplicate)
+
+for (const lang of setOfLanguages) {
+    let result = languagesCountWithDuplicate.filter(ele => ele === lang)
+    let obj = {
+        language: lang,
+        count: result.length
+    }
+    actualLanguageCount.push(obj)
+    actualLanguageCount.sort((a, b) => {
+        return b.count - a.count
+    })
+}
+
+lang.addEventListener('click', () => {
+    visual.innerHTML = ''
+    let topTen = actualLanguageCount.slice(0, 10)
+    let total = topTen[0].count
+    for (let ele of topTen) {
+        let newDiv = document.createElement('div')
+        let newH2 = document.createElement('h2')
+        let p1 = document.createElement('p')
+        let p2 = document.createElement('p')
+        newH2.textContent = ele.language
+        newH2.className = 'name'
+        let width = rtnPercentage(ele.count, total)
+        p1.style.width = `${width}%`
+        p1.className = 'bar'
+        p2.textContent = ele.count
+        p2.className = 'count'
+        newDiv.appendChild(newH2)
+        newDiv.appendChild(p1)
+        newDiv.appendChild(p2)
+        visual.appendChild(newDiv)
+    }
+    countStatus.textContent = `${setOfLanguages.size} Languages`
+    btnStatus.textContent = '10 Most Spoken Languages in the world'
 })
