ad42b30cbf4b6800a4555d35dfbe07cb0d71c20a
Aravindray <aravindselvasan@gmail.com>
2024-06-04 19:43:09 +0530

    working on leader board

---
+++ Asabeneh/28_Leaderboard/28_leaderboard.js
@@ -0,0 +1,26 @@
+const users = [
+    {
+        firstName: 'Rich',
+        lastName: 'Bell',
+        country: 'USA',
+        score: 80,
+        userId: 1,
+        createdDate: 'Jan 13, 2024 23:12',
+    },
+    {
+        firstName: 'Aravind',
+        lastName: 'Ray',
+        country: 'Fiji',
+        score: 70,
+        userId: 2,
+        createdDate: 'May 16, 2024 12:12',
+    },
+    {
+        firstName: 'Daniel',
+        lastName: 'Raj',
+        country: 'Fiji',
+        score: 99,
+        userId: 3,
+        createdDate: 'Jun 12, 2023 21:18',
+    },
+]
