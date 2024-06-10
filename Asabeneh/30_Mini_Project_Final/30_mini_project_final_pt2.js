1d874590013728956eea2214d2b2dd29dbbac0da
Aravindray <aravindselvasan@gmail.com>
2024-06-10 16:57:51 +0530

    working on form validation

--- Asabeneh/30_Mini_Project_Final/30_mini_project_final_pt2.js
+++ Asabeneh/30_Mini_Project_Final/30_mini_project_final_pt2.js
@@ -7,16 +7,38 @@ let password = document.querySelector('#password')
 let tel = document.querySelector('#tele')
 let bio = document.querySelector('#your-bio')
 let fnTrowEor = document.querySelector('.fn-trowEor')
+let lnTrowEor = document.querySelector('.ln-trowEor')
+let emTrowEor = document.querySelector('.em-trowEor')
 
-let namePattern = /^[A-Z]{1}[\w]{2,16}/g
+let namePattern = /\w{3,16}/g
 
 firstName.addEventListener('input', (e)=> {
     let value = e.target.value
-    console.log(value)
     let result = namePattern.test(value)
-    console.log(result)
     if (result) {
         fnTrowEor.style.display = 'none'
-        firstName.style.borderColor = 'green'
+        firstName.style.border = '3px solid lime'
+    }
+})
+
+lastName.addEventListener('input', (e)=> {
+    let value = e.target.value
+    let result = namePattern.test(value)
+    if (result) {
+        lnTrowEor.style.display = 'none'
+        lastName.style.border = '3px solid lime'
+    }
+})
+
+let emailPattern = /^[a-z0-9._-%]+\@[a-z0-9.-_]+\.[a-z]{2,4}$/gi
+
+email.addEventListener('input', (e) => {
+    let value = e.target.value
+    let result = emailPattern.test(value)
+    if (result) {
+        emTrowEor.style.display = 'none'
+        email.style.border = '3px solid lime'
+    } else {
+        email.style.border = '3px solid orange'
     }
 })
